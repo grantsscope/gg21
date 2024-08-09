470a6a20c9c37d32ff0e4ba0ac57c76d8a06b566
rohitmalekar <rohitmalekar@gmail.com>
2024-08-10 02:39:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 import psycopg2 as pg
 
-DB_HOST = '137.184.237.13'
-DB_PORT = 5432
-DB_NAME = 'Grants'
-DB_USERNAME = 'sybilhunter'
-DB_PASSWORD = 'hunterxhunter420'
+
 
 def fetch_data(query, params=None):
     conn = conn = pg.connect(host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
