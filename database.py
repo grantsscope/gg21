b6d88121892866523dfd20b68463f64cd2963f04
rohitmalekar <rohitmalekar@gmail.com>
2024-08-10 03:00:15 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import psycopg2 as pg
+from sqlalchemy import create_engine
 
 DB_HOST= st.secrets['DB_HOST']
 DB_PORT = st.secrets['DB_PORT']
@@ -9,13 +8,12 @@ DB_NAME = st.secrets['DB_NAME']
 DB_USERNAME = st.secrets['DB_USERNAME']
 DB_PASSWORD = st.secrets['DB_PASSWORD']
 
+engine = create_engine(f'postgresql+pg8000://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
 def fetch_data(query, params=None):
-    conn = conn = pg.connect(host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
-    try:
+    with engine.connect() as conn:
         if params:
-        	results = pd.read_sql_query(query, conn, params)    
+            results = pd.read_sql_query(query, conn, params=params)
         else:
-            results = pd.read_sql_query(query, conn)    
+            results = pd.read_sql_query(query, conn)
         return results
-    finally:
-        conn.close()
